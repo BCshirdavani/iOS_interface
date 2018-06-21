fab0972811164bbd8e5483bec2a8830a96d22af3
shiMac <beau.shirdavani@bellevuecollege.edu>
2018-06-21 14:40:02 -0700

    images imported
    drag and drop in the Assets.scassets folder of navigation pane.

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by shiMac on 6/21/18.
+//  Copyright © 2018 shiMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
